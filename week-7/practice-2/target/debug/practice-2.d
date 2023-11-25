60b99360da8226fa10e7a157ea218b499d44d44e
Kami-no-Yujin <nnamdiebunam15@gmail.com>
2023-11-25 15:47:10 +0100

    (week-7)

---
+++ week-7/practice-2/target/debug/practice-2.d
@@ -0,0 +1,1 @@
+C:\Users\nnamd\Fuji.kazeCOS101\week-7\practice-2\target\debug\practice-2.exe: C:\Users\nnamd\Fuji.kazeCOS101\week-7\practice-2\src\main.rs
---
+++ week-7/practice-7/target/debug/deps/practice_7.d
@@ -0,0 +1,5 @@
+C:\Users\nnamd\Fuji.kazeCOS101\week-7\practice-7\target\debug\deps\practice_7.exe: src\main.rs
+
+C:\Users\nnamd\Fuji.kazeCOS101\week-7\practice-7\target\debug\deps\practice_7.d: src\main.rs
+
+src\main.rs:
